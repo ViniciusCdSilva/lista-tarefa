685716ca63c77f068e7c407d2b7d020a8b8286e8
Vinicius Silva <viniciuscassianosilva13@gmail.com>
2025-03-17 23:47:26 -0300

    opção 2 e 3 feitas
    adicionado status e numeração e mudar o status

--- index.py
+++ index.py
@@ -1,15 +1,31 @@
 class listarefa:
     def __init__(self):
-        self.tarefa =[]
+        self.tarefas=[]
 
     def add_tarefas(self,tarefa):
-        self.tarefa.append({"tarefa":tarefa,"status": "pendente"})
+        self.tarefas.append({"tarefa":tarefa,"status": "pendente"})
         print(f"Tarefa{tarefa}adicionada com sucesso!")
+
+    def list_tarefas(self):
+        if not self.tarefas :
+            print("nenhuma tarefa cadastrada")
+            return
+        print("Listas de tarefas")
+        for n, tarefa in enumerate(self.tarefas , start=1):
+            status = tarefa["status"]
+            print(f"{n}.{tarefa['tarefa']}-{status}")
+
+    def conc_tarefa(self,indice):
+        if 0 < indice <= len(self.tarefas):
+            self.tarefas[indice - 1]["status"]="Concluída"
+            print(f"Tarefa{self.tarefas[indice - 1]['tarefas']} concluída com sucesso!")
+
 
 
 
 
 def menu():
+    lista = listarefa()
     while True :
         print("Menu")
         print("\n 1. Adicionar tarefa")
@@ -22,8 +38,18 @@ def menu():
 
         if opcao == "1":
             tarefa = input("Digite a sua tarefa")
-            lista.add_tarefa(tarefa)
+            lista.add_tarefas(tarefa)
             print ("\n Adicionar tarefa ")
+
+        elif opcao == "2":
+            lista.list_tarefas()
+
+        elif opcao=="3":
+            try:
+                indice =int(input("Digite o numero de tarefas que desja concluir: "))
+                lista.conc_tarefa(indice)
+            except ValueError:
+                print("Digite um numero valido ")       
         
 menu()        
         
